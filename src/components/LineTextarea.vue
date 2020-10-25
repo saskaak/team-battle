1425b3a5eb104ba39d497396916ca111bd627ea1
saska <saska@oeoe.fi>
2020-10-25 13:53:22 +0200

    add LineTextArea component

---
+++ src/components/LineTextarea.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="LineTextarea">
+    <div class="LineTextarea__placeholder">
+      <br
+        v-for="line in modelValue"
+        :key="line"
+      />
+      {{placeholder}}
+    </div>
+    <textarea
+      v-bind="attrsTextarea"
+      :value="string"
+      :rows="modelValue.length + 1"
+      class="LineTextarea__textarea"
+      @input="onInput"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: Array,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    attrsTextarea: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    stringRaw() {
+      return this.modelValue.join('\n');
+    },
+    string() {
+      return this.stringRaw.length === 0
+        ? ''
+        : `${this.stringRaw}\n`;
+    }
+  },
+  methods: {
+    async onInput(event) {
+      console.log('onInput');
+
+      const el = event.target;
+      const {value} = el;
+      if (!value) {
+        this.$emit('update:modelValue', []);
+      }
+
+      // Remove all extra newlines
+      const valueFiltered = value
+        .replace(/^\n/g, '')
+        .replace(/\n\s*\n/g, '\n');
+      const array = valueFiltered
+        .split('\n')
+        .filter(line => line.trim());
+
+      this.$emit('update:modelValue', array);
+
+      // Correct the caret position after adding an extra newline in computed.string:
+      const caretPosition = el.selectionStart;
+      await this.$nextTick();
+      if (this.stringRaw === valueFiltered) {
+        el.setSelectionRange(caretPosition, caretPosition);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~@/styles/definitions";
+
+.LineTextarea {
+}
+
+.LineTextarea__placeholder {
+  position: absolute;
+  user-select: none;
+  opacity: 0.5;
+}
+
+.LineTextarea__textarea {
+  position: relative;
+  display: block;
+  width: 100%;
+  max-width: 100%;
+  margin: 0;
+  padding: 0;
+  border: none;
+  font: inherit;
+  color: inherit;
+  background: none;
+  resize: none;
+}
+
+</style>
