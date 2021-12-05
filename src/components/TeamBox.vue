11a344f7970df1d5e482f323d62e685fd3c230b1
saska <saska@oeoe.fi>
2021-12-05 11:12:17 +0200

    extract the styles from TeamInput to a separate TeamBox component

---
+++ src/components/TeamBox.vue
@@ -0,0 +1,77 @@
+<template>
+  <component
+    :is="is"
+    :class="`TeamBox--${color}`"
+    class="TeamBox"
+  >
+    <span class="TeamBox__label">{{ color }}</span>
+    <div class="TeamBox__main">
+      <slot/>
+    </div>
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    is: {
+      type: String,
+      default: 'div',
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~@/styles/definitions";
+
+.TeamBox {
+  display: flex;
+  align-items: center;
+  border-radius: r(24);
+  box-shadow: 0 r(8) r(24) r(8) rgba($color-black, 0.1);
+  background-color: $color-white;
+  padding-left: r(32);
+}
+
+.TeamBox__label {
+  position: absolute;
+  display: block;
+  margin-left: r(-16);
+  transform: translateX(-50%) rotate(270deg);
+  font-size: r(12);
+  letter-spacing: r(2);
+  text-transform: uppercase;
+
+  .TeamBox--red & {
+    color: $color-red;
+  }
+
+  .TeamBox--blue & {
+    color: $color-blue;
+  }
+}
+
+.TeamBox__main {
+  flex-grow: 1;
+  padding: r(32);
+  border-radius: r(24);
+  font-family: $ff-grandstander;
+  font-weight: bold;
+  font-size: r(20);
+  line-height: r(32);
+  color: $color-white;
+
+  .TeamBox--red & {
+    background: $gradient-corner-red;
+  }
+
+  .TeamBox--blue & {
+    background: $gradient-corner-blue;
+  }
+}
+</style>
--- src/components/TeamInput.vue
+++ src/components/TeamInput.vue
@@ -1,25 +1,27 @@
 <template>
-  <label
+  <TeamBox
+    :color="color"
+    is="label"
     class="TeamInput"
-    :class="`TeamInput--${color}`"
   >
-    <span class="TeamInput__label">{{ color }}</span>
     <LineTextarea
       v-model="emitterModelValue"
-      class="TeamInput__line-textarea"
+      class="TeamInput__input"
       placeholder="Enter a character..."
       min-rows="4"
-      :attrsTextarea="{class: 'TeamInput__line-textarea-textarea', spellcheck: false}"
+      :attrsTextarea="{class: 'TeamInput__textarea', spellcheck: false}"
     />
-  </label>
+  </TeamBox>
 </template>
 
 <script>
 import LineTextarea from '@/components/LineTextarea';
 import vModelEmitter from '@/lib/vModelEmitter';
+import TeamBox from '@/components/TeamBox';
 
 export default {
   components: {
+    TeamBox,
     LineTextarea,
   },
   props: {
@@ -42,56 +44,9 @@ export default {
 @import "~@/styles/definitions";
 
 .TeamInput {
-  display: flex;
-  align-items: center;
-  border-radius: r(24);
-  box-shadow: 0 r(8) r(24) r(8) rgba($color-black, 0.1);
-  background-color: $color-white;
-  padding-left: r(32);
 }
 
-.TeamInput__label {
-  position: absolute;
-  display: block;
-  margin-left: r(-16);
-  transform: translateX(-50%) rotate(270deg);
-  font-size: r(12);
-  letter-spacing: r(2);
-  text-transform: uppercase;
-
-  .TeamInput--red & {
-    color: $color-red;
-  }
-
-  .TeamInput--blue & {
-    color: $color-blue;
-  }
-}
-
-.TeamInput__line-textarea {
-  flex-grow: 1;
-  padding: r(32);
-  border-radius: r(24);
-  font-family: $ff-grandstander;
-  font-weight: bold;
-  font-size: r(20);
-  line-height: r(32);
-  color: $color-white;
-
-  &:focus {
-    outline: none;
-  }
-
-  .TeamInput--red & {
-    background: $gradient-corner-red;
-  }
-
-  .TeamInput--blue & {
-    background: $gradient-corner-blue;
-  }
-}
-
-.TeamInput__line-textarea-textarea {
+.TeamInput__textarea {
   &:focus {
     outline: none;
   }
