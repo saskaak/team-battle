0e79c855d8b8da8468524e7fc8d3ff2f29fb2b0c
saska <saska@oeoe.fi>
2022-04-19 14:31:01 +0300

    rewrite the info "article"

--- src/components/ArticleInfo.vue
+++ src/components/ArticleInfo.vue
@@ -3,14 +3,14 @@
     <div class="text__inner">
       <h1>What is a team battle?</h1>
       <p>
-        Team battle is a fighting game set variant that can be played in any 1-on-1 game with multiple different
+        Team battle is a fighting game set variant that can be played in any 1-on-1 game that has multiple different
         characters. Before starting, each player creates a team of two or more characters. Then, individual games are
-        played where the winning character is awarded a crown. This continues until a player wins by crowning all of
-        their characters. The catch? The characters are picked randomly for each match.
+        played where the winning character is awarded a crown. This continues until either player has crowned all of
+        their characters and wins the battle. The catch? The characters are picked randomly for each match.
       </p>
       <p>
-        After playing a character, it's removed from the random pool. If a player has no available characters left, all
-        non-crowned characters become available again.
+        After playing a character, it's removed from a pool of available characters. If a player has no available
+        characters left, all non-crowned characters are returned to back the pool.
       </p>
     </div>
   </div>
