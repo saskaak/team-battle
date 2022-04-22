99d0b9fdada29d4129cdf6923de0824c1b6f3dc2
saska <saska@oeoe.fi>
2022-04-22 09:43:14 +0300

    add a credit to Fantasy Strike

--- src/components/ArticleInfo.vue
+++ src/components/ArticleInfo.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="text">
     <div class="text__inner">
-      <h1>What is a team battle?</h1>
+      <h1>What is a Team Battle?</h1>
       <p>
-        Team battle is a fighting game set variant that can be played in any 1-on-1 game that has multiple different
-        characters. Before starting, each player creates a team of two or more characters. Then, individual games are
-        played where the winning character is awarded a crown. This continues until either player has crowned all of
-        their characters and wins the battle. The catch? The characters are picked randomly for each match.
+        Team Battle, originally a game mode in
+        <a href="https://www.fantasystrike.com/">Fantasy Strike</a>,
+        is a fighting game set variant that can be played in any 1&#8209;on&#8209;1 fighting game with multiple
+        different characters.
+      </p>
+      <p>
+        Before starting, each player creates a team of two or more characters. Then, individual games are played where
+        the winning character is awarded a crown. This continues until either player has crowned all of their characters
+        and wins the battle. The catch? The characters are picked randomly for each match.
       </p>
       <p>
         After playing a character, it's removed from a pool of available characters. If a player has no available
